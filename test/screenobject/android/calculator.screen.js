a77a1f65f5d73de76a2d73d08aebd9cac824b801
Jamil Kawsher <jamilkawsher@gmail.com>
2023-10-09 21:38:00 +0600

    basic asserion added with Screen Object Model

--- test/screenobject/android/calculator.screen.js
+++ test/screenobject/android/calculator.screen.js
@@ -1,21 +1,21 @@
-class CalculatorHomePage{
+class CalculatorHomePage {
+  get btn_1() {
+    return $('//*[@resource-id="com.google.android.calculator:id/digit_1"]');
+  }
 
-    get btn_1(){
+  get btn_2() {
+    return $('//*[@resource-id="com.google.android.calculator:id/digit_2"]');
+  }
 
+  get btn_plus() {
+    return $('//*[@resource-id="com.google.android.calculator:id/op_add"]');
+  }
 
-        return $('//*[@resource-id="com.google.android.calculator:id/digit_1"]')
-    }
-
-    get btn_2(){
-
-
-        return $('//*[@resource-id="com.google.android.calculator:id/digit_2"]')
-    }
-
-    get btn_plus(){
-
-        return $('//*[@resource-id="com.google.android.calculator:id/op_add"]')
-    }
+  get result_preview() {
+    return $(
+      '//*[@resource-id="com.google.android.calculator:id/result_preview"]'
+    );
+  }
 }
 
-module.exports=new CalculatorHomePage();
+module.exports = new CalculatorHomePage();
--- test/specs/sample.js
+++ test/specs/sample.js
@@ -1,23 +1,20 @@
 // const calcHomepage=require("../pageobjects/calcHomepage")
-const calculatorScreen = require("../screenobject/android/calculator.screen")
+const calculatorScreen = require("../screenobject/android/calculator.screen");
 
+describe("Smaple", () => {
+  it("sample Test", async () => {
+    // await  $('//*[@resource-id="com.google.android.calculator:id/digit_7"]').click();
 
+    // await $('//*[@resource-id="com.google.android.calculator:id/digit_6"]').click()
 
+    // await calcHomepage.btn_1.click()
+    // await  calcHomepage.btn_1.click();
+    // await calcHomepage.btn_1.click();
 
-describe("Smaple",()=>{
-
-    it("sample Test",async()=>{
-
-      // await  $('//*[@resource-id="com.google.android.calculator:id/digit_7"]').click();
-
-      // await $('//*[@resource-id="com.google.android.calculator:id/digit_6"]').click()
-
-      // await calcHomepage.btn_1.click()
-      // await  calcHomepage.btn_1.click();
-      // await calcHomepage.btn_1.click();
-
-      await calculatorScreen.btn_1.click()
-      await calculatorScreen.btn_plus.click()
-      await calculatorScreen.btn_2.click()
-    })
-})
+    await calculatorScreen.btn_1.click();
+    await calculatorScreen.btn_plus.click();
+    await calculatorScreen.btn_2.click();
+    const resultvalue = await calculatorScreen.result_preview.getText();
+    await expect(resultvalue).toEqual("3");
+  });
+});
